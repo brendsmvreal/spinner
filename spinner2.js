8f04a1ad8ac1754be01929b60ad3880372b29b20
Brenda <brendamvreal@gmail.com>
2022-02-22 13:25:36 -0500

    Remove comment

--- spinner2.js
+++ spinner2.js
@@ -1,35 +1,35 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
 
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
 
-setTimeout(() => {
-  process.stdout.write('\r\|   ');
-}, 800);
+// setTimeout(() => {
+//   process.stdout.write('\r\|   ');
+// }, 800);
 
-setTimeout(() => {
-  process.stdout.write('\r\/   ');
-}, 900);
+// setTimeout(() => {
+//   process.stdout.write('\r\/   ');
+// }, 900);
 
-setTimeout(() => {
-  process.stdout.write('\r\-  ');
-}, 900);
+// setTimeout(() => {
+//   process.stdout.write('\r\-  ');
+// }, 900);
 
-setTimeout(() => {
-  process.stdout.write('\r\\  ');
-}, 1000);
+// setTimeout(() => {
+//   process.stdout.write('\r\\  ');
+// }, 1000);
 
 let time = 100;
 let spins = ['\r|   ', '\r/   ','\r-   ', '\r\\   ','\r\|   ','\r\/   ','\r\-  ','\r\\  '];
